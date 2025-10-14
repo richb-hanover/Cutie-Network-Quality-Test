1bb8bd54d2e7249b8fb3e1aee76a82f661d7165b
Rich Brown <richb.hanover@gmail.com>
2025-10-13 21:17:54 -0400

    SvelteKit starting to work; Added intro.svelte; Tuned up +layout.svelte, +page.svelte, app.css, tsconfig.json;

---
+++ src/lib/Intro.svelte
@@ -0,0 +1,27 @@
+<!-- src/lib/Intro.svelte -->
+<script lang="ts">
+	// export let title: string = 'Welcome';
+	// export let message: string = 'This is the intro section.';
+</script>
+
+<section class="intro">
+	<script src="index.js"></script>
+	<h1>
+		NST Network Stability Test<br /><small><i>(v0.0.1)</i></small>
+	</h1>
+	<p id="descriptionMessage"></p>
+	<p>Please click on the <b>Start button</b> to begin running this test.</p>
+
+	<button>Start</button>
+	<button id="stopButton" disabled="">Stop</button>
+	<button id="saveButton" disabled="">Save as image</button>
+
+	<p id="summaryMessage"></p>
+	<p id="divTag"></p>
+	<p id="overallMessage"></p>
+	<canvas id="MOSChart"></canvas>
+	<canvas id="lossChart"></canvas>
+	<canvas id="delayChart"></canvas>
+	<p id="consoleMessage"></p>
+	<script src="data.js"></script>
+</section>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,11 @@
+
+<script lang="ts">
+  import Intro from '$lib/Intro.svelte';
+  let user = 'Rich';
+  let clicks = 5;
+</script>
+
+<Intro name={user} count={clicks} />
+
 <h1>Welcome to SvelteKit</h1>
 <p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
